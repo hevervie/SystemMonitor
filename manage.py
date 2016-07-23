2842ac28a3f8a9c3edb3c2bcdaaf4feaf660269d
dreamer2018 <zhoupans_mail@163.com>
2016-07-23 15:37:21 +0800

    系统配置管理文件，读取配置文件

--- manage.py
+++ manage.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 '''
     Created by zhoupan on 7/23/16.
 '''
-from 
+
+import configparser
+
+
+class ReadConf():
+    "读取配置文件"
+
+    def __init__(self):
+        "类初始化"
+        pass
+
+    def read_conf(self):
+        "读取配置文件"
+
+        cf = configparser.ConfigParser()
+        cf.read("client.conf")
+        self.port = cf.getint("server", "port")
+        self.host = cf.get("server", "host")
+        self.buf_size = cf.getint("buffer","size")
+
+if __name__ == '__main__':
+    rc = ReadConf()
+    rc.read_conf()
+    print(rc.buf_size)
+    print(rc.host)
+    print(rc.port)
