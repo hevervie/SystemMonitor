a00e36449362887696e78f5434a2ad862b16b3d6
dreamer2018 <zhoupans_mail@163.com>
2016-07-23 16:12:05 +0800

    客户端

---
+++ Client.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding: UTF-8 -*-
+'''
+    Created by zhoupan on 7/23/16.
+'''
+
+from socket import *
+import threading
+
+
+class Clinet(threading.Thread):
+    def __init__(self,threadID,threadName):
+        threading.Thread.__init__(self)
+        threading.threadID=threadID
+        threading.name=threadName
+    def run(self):
+        tcpClinet=socket(AF_INET,SOCK_STREAM)
+        tcpClinet.connect(ADDR)
+        while True:
+            data=input('请输入：')
+            tcpClinet.send(data.encode())
+            data=tcpClinet.recv(BUFSIZE).decode()
+            if not data:
+                break
+            print(data)
+        tcpClinet.close()
+if __name__ == '__main__':
+    thread=Clinet(1,'first')
+    thread.start()
