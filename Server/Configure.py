103f197bade677c66c4cf4e883729632df9bd484
dreamer2018 <zhoupans_mail@163.com>
2016-07-30 16:23:13 +0800

    代码重构

--- Server/Configure.py
+++ Server/Configure.py
@@ -1,29 +1,48 @@
 #!/usr/bin/env python
-#-*- coding: UTF-8 -*-
-'''
+# -*- coding: UTF-8 -*-
+
+"""
     Created by zhoupan on 7/23/16.
-'''
+"""
 
 import configparser
 
-class ReadConf():
-    "读取配置文件"
+
+class Configure():
+    """读取配置文件"""
 
     def __init__(self):
-        "类初始化"
+        """类初始化"""
+        pass
 
-        "读取配置文件"
+    def read_config(self, conf_name, section, option):
+        """读取配置文件"""
+        value = ""
+        try:
+            cf = configparser.ConfigParser()
+            cf.read(conf_name)
+            value = cf.get(section, option)
+        except Exception:
+            pass
+        finally:
+            return value
+
+    def write_config(self, conf_name, value):
+        """写入配置文件"""
+
+        # 打开文件
+        fp = open(conf_name, 'w')
         cf = configparser.ConfigParser()
-        cf.read("server.conf")
-        self.port = cf.getint("server", "port")
-        self.host = cf.get("server", "host")
-        self.buf_size = cf.getint("buffer","size")
-        self.max_len = cf.getint("server","max_len")
+        # 遍历字典，并写入文件
+        for (k, v) in value.items():
+            cf.add_section(str(k))
+            for (e, a) in v.items():
+                cf.set(str(k), str(e), str(a))
+        cf.write(fp)
+        fp.close()
+
 
 if __name__ == '__main__':
-    rc = ReadConf()
-   # rc.read_conf()
-    print(rc.max_len)
-    print(rc.buf_size)
-    print(rc.host)
-    print(rc.port)
+    c = Configure()
+    print(c.write_config('1.conf', {'client': {}, 'server': {'port': 8000, 'host': '192.168.30.2'}}))
+    print(c.read_config('1.conf', 'server', 'port'))
