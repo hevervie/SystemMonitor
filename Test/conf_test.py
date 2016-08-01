2d892294deb5676ab1e109db02d9e2cfe81b9eb7
dreamer2018 <zhoupans_mail@163.com>
2016-08-01 15:34:05 +0800

    配置文件测试

--- Test/conf_test.py
+++ Test/conf_test.py
@@ -3,3 +3,14 @@
 """
     Created by zhoupan on 8/1/16.
 """
+import configparser
+
+cf = configparser.ConfigParser()
+cf.read('1.conf')
+value = cf.get('user','user')
+print(value)
+print(type(value))
+value=tuple(eval(value))
+print(type(value))
+print(value)
+print(value.__str__())
