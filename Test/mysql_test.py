d5cf924f4dcb0ec41adcaf2adbc2c39f29034ed9
dreamer2018 <zhoupans_mail@163.com>
2016-08-04 23:38:18 +0800

    python连接数据库测试

--- Test/mysql_test.py
+++ Test/mysql_test.py
@@ -1,5 +1,14 @@
 #!/usr/bin/env python
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 """
     Created by zhoupan on 8/4/16.
 """
+import pymysql
+
+conn = pymysql.connect(host='localhost', user='root', passwd='root', db='SysMon', port=3306, charset='utf8')
+cur = conn.cursor()
+cur.execute('show tables')
+d1 = cur.fetchall()
+d2 = cur.f
+print(d1)
+print(d2)
