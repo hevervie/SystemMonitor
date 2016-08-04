bd8d55d2df4d3f80a940a583b9a38085aeba979b
dreamer2018 <zhoupans_mail@163.com>
2016-08-04 23:36:53 +0800

    增加持久化层

--- Server/Persistent.py
+++ Server/Persistent.py
@@ -3,6 +3,9 @@
 """
     Created by zhoupan on 8/4/16.
 """
+# 导入mysql的包
+import pymysql
+from Configure import Configure
 
 
 class Persistent():
@@ -10,101 +13,175 @@ class Persistent():
 
     def __init__(self):
         """类初始化"""
-        pass
+        cf = Configure()
+        self.host = cf.read_config('server.conf', 'mysql', 'host')
+        self.port = int(cf.read_config('server.conf', 'mysql', 'port'))
+        self.db = cf.read_config('server.conf', 'mysql', 'database')
+        self.user = cf.read_config('server.conf', 'mysql', 'user')
+        self.passwd = cf.read_config('server.conf', 'mysql', 'passwd')
 
     def save_all_data(self, data, addr):
         """保存所有的原数据"""
-
         index = []
+        conn = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db, port=self.port,
+                               charset='utf8')
+        cur = conn.cursor()
 
         # client 客户端列表
         # 找出客户端是否存在
-        sql = "SELECT count(id) FROM client WHERE host = \'%s\';" % addr
-        print(sql)
-        # 新建新的客户端数据
-        sql = "INSERT INTO client(host) VALUES (\'%s\');" % addr
-        print(sql)
+        sql = "SELECT count(id),id FROM client WHERE host = \'%s\';" % addr
+
+        # 执行sql语句
+        cur.execute(sql)
+        # 获取结果
+        result = cur.fetchall()
+
+        # 如果此客户端不存在
+        if result[0][0] == 0:
+            # 新建新的客户端数据
+            sql = "INSERT INTO client(host) VALUES (\'%s\');" % addr
+            cur.execute(sql)
+            # 将运行结果提交
+            index.append(conn.insert_id())
+            conn.commit()
+        # 如果存在，则将此id记录下来
+        else:
+            index.append(result[0][1])
 
         # scputimes
         d = data[0]
-        sql = "INSERT INTO scputimes(user,nice,system,idle,iowate,irq,softirq,steal,guest,guestnice) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
+        sql = "INSERT INTO scputimes(user,nice,system,idle,iowait,irq,softirq,steal,guest,guest_nice) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
             d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9])
-        print(sql)
+        cur.execute(sql)
+        index.append(conn.insert_id())
+        conn.commit()
 
         # svmem
         d = data[1][0]
         sql = "INSERT INTO svmem(total,available,precent,used,free,active,inactive,buffers,cached,shared) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
             d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8], d[9])
-        print(sql)
+        cur.execute(sql)
+        index.append(conn.insert_id())
+        conn.commit()
 
         # sswap
         d = data[1][1]
         sql = "INSERT INTO sswap(total,used,free,precent,sin,sout) VALUES (%s,%s,%s,%s,%s,%s);" % (
             d[0], d[1], d[2], d[3], d[4], d[5])
-        print(sql)
+        cur.execute(sql)
+        index.append(conn.insert_id())
+        conn.commit()
 
         # sdiskio
         d = data[2][0]
-        sql = "INSERT INTO sdiskio(read_count,write_count,read_bytes,write_byes,read_time,write_times,read_merged_count,write_merged_count,busy_time) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
+        sql = "INSERT INTO sdiskio(read_count,write_count,read_bytes,write_bytes,read_time,write_time,read_merged_count,write_merged_count,busy_time) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
             d[0], d[1], d[2], d[3], d[4], d[5], d[6], d[7], d[8])
-        print(sql)
+        cur.execute(sql)
+        index.append(conn.insert_id())
+        conn.commit()
 
         # sdiskusage
         d = data[2][1]
         sql = "INSERT INTO sdiskusage(point,total,used,free,precent) VALUES (\'%s\',%s,%s,%s,%s);" % (
             "/", d[0], d[1], d[2], d[3])
-        print(sql)
+        cur.execute(sql)
+        index.append(conn.insert_id())
+        conn.commit()
 
         # snetio
         d = data[3]
         # 获取type最大值
         sql = "select Max(type) from snetio;"
+        cur.execute(sql)
+        result = cur.fetchall()
         type = 0
-        print(sql)
+        if result[0][0] == None or result[0][0] == 0:
+            type = 1
+        else:
+            type = result[0][0] + 1
         for k, v in d.items():
-            sql = "INSERT INTO snetio(device,type,bytes_sent,bytes_recv,package_sent,package_recv,errin,errout,busy_time) VALUES (\'%s\',%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
+            sql = "INSERT INTO snetio(device,type,bytes_sent,bytes_recv,packets_sent,packets_recv,errin,errout,dropin,dropout) VALUES (\'%s\',%s,%s,%s,%s,%s,%s,%s,%s,%s);" % (
                 k, type, v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7])
-            print(sql)
-
+            cur.execute(sql)
+            conn.commit()
+        index.append(type)
         # suser
         d = data[4]
         # 获取type最大值
         sql = "select Max(type) from suser;"
+        cur.execute(sql)
+        result = cur.fetchall()
         type = 0
+        if result[0][0] == None or result[0][0] == 0:
+            type = 1
+        else:
+            type = result[0][0] + 1
         for v in d:
             sql = "INSERT INTO suser(type,user,terminal,host,started) VALUES (%s,\'%s\',\'%s\',\'%s\',%s);" % (
                 type, v[0], v[1], v[2], v[3])
-            print(sql)
+            cur.execute(sql)
+            conn.commit()
+        index.append(type)
 
         # sport
         d = data[5]
         # 获取type最大值
-        sql = "select Max(type) from suser;"
-        print(sql)
+        sql = "select Max(type) from sport;"
         type = 0
+        cur.execute(sql)
+        result = cur.fetchall()
+        if result[0][0] == None or result[0][0] == 0:
+            type = 1
+        else:
+            type = result[0][0] + 1
         for v in d:
             sql = "INSERT INTO sport(type,port) VALUES (%s,%s)" % (type, v)
-            print(sql)
+            cur.execute(sql)
+            conn.commit()
+        index.append(type)
+        print(index)
 
-        # receive
-        sql = "INSERT INTO receive(client_id,cpu_id,svmem_id,swap_id,diskio_id,diskusage_id,netio_id,user_id,port_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (
+        # # receive
+        sql = "INSERT INTO receive(client_id,cpu_id,svmem_id,swap_id,diskio_id,diskusage_id,netio_type,user_type,port_type) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (
             index[0], index[1], index[2], index[3], index[4], index[5], index[6], index[7], index[8])
+        cur.execute(sql)
+        conn.commit()
+        conn.close()
 
     def save_alarm_data(self, data, addr):
         """保存处理过的告警数据"""
 
+        # 连接数据库
+        conn = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db, port=self.port,
+                               charset='utf8')
+        # 获取游标
+        cur = conn.cursor()
+
         # 找出客户端
-        sql = "SELECT id,count(id) FROM client WHERE host = \'%s\';" % addr
-        print(sql)
-        index=0
-        # 找出数据的id
-        sql = "SELECT max(id) FROM receive WHERE client_id = %s " % (index)
-        print(sql)
-
-
-        #
-        # sql = "INSERT INTO alarm(client_id,cpu_id,svmem_id,swap_id,diskio_id,diskusage_id,netio_id,user_id,port_id) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (
-        #     index[0], index[1], index[2], index[3], index[4], index[5], index[6], index[7], index[8])
+        sql = "SELECT count(id),id FROM client WHERE host = \'%s\';" % addr
+        cur.execute(sql)
+        result = cur.fetchall()
+        if result[0][0] == 0:
+            # 新建新的客户端数据
+            sql = "INSERT INTO client(host) VALUES (\'%s\');" % addr
+            cur.execute(sql)
+            # 将运行结果提交
+            conn.commit()
+        else:
+            index = result[0][1]
+            # 找出数据的id
+            sql = "SELECT max(id),id FROM receive WHERE client_id = %s " % (index)
+            cur.execute(sql)
+            result = cur.fetchall()
+            if result[0][0] == None or result[0][0] == 0:
+                pass
+            else:
+                recv = result[0][0]
+                sql = "INSERT INTO alarm(recv_id,client_id,cpu,svmem,swap,diskio,diskusage,snetio,suser,port,level) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s)" % (
+                    recv, index, data[0], data[1], data[2], data[3], data[4], data[5], data[6])
+                cur.execute(sql)
+                # 将运行结果提交
+                conn.commit()
 
 
 if __name__ == '__main__':
@@ -122,4 +199,4 @@ if __name__ == '__main__':
                 '902'))
     p = Persistent()
     # p.save_all_data(data, '192.168.30.8')
-    p.save_alarm_data(data,'192.168.30.8')
+    p.save_alarm_data(data, '192.168.30.8')
