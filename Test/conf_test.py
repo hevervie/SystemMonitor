846cde51a16ebc3d9bb5863215d3a583a48e331e
dreamer2018 <zhoupans_mail@163.com>
2016-08-05 16:40:13 +0800

    add' “

--- Test/conf_test.py
+++ Test/conf_test.py
@@ -4,13 +4,21 @@
     Created by zhoupan on 8/1/16.
 """
 import configparser
+from Server.Configure import Configure
+
 
 cf = configparser.ConfigParser()
 cf.read('1.conf')
 value = cf.get('user','user')
-print(value)
-print(type(value))
-value=tuple(eval(value))
-print(type(value))
-print(value)
-print(value.__str__())
+# print(value)
+# print(type(value))
+# value=tuple(eval(value))
+# print(type(value))
+# print(value)
+# print(value.__str__())
+
+cf = Configure()
+dict = cf.read_config('../Server/strategy.conf', 'mail', 'mail')
+print(dict)
+print(eval(dict))
+print(type(eval(dict)))
