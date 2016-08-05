9324224264b4ca5adc10195a3981698b2900165d
dreamer2018 <zhoupans_mail@163.com>
2016-08-05 15:53:58 +0800

    完成告警类

--- Server/Alarm.py
+++ Server/Alarm.py
@@ -3,19 +3,20 @@
 """
     Created by yangliru,zhoupan on 8/1/16.
 """
-import smtplib
-from email.header import Header
-from email.mime.text import MIMEText
 
 from Configure import Configure
 
 
 class Alarm():
-    def __init__(self, mail):
+    def __init__(self):
+        """类初始化工作"""
+        cf = Configure()
+        dict = cf.read_config('strategy.conf', 'mail', 'mail')
+
         self.server = "smtp.mailgun.org"  # 设置服务器
         self.user = "ru@raydina.me"  # 用户名
         self.passwd = "823264073"  # 口令
-        self.mail = mail  # 告警用户等级字典
+        self.mail = eval(dict)  # 告警用户等级字典
 
     def send_list(self, level):
         match_data = []
@@ -24,22 +25,23 @@ class Alarm():
                 match_data.append(value)
         return match_data
 
-    def send_mail(self, level, message):
+    def send_mail(self, level, ms):
         mail_tuple = self.send_list(level)
         for i in range(len(mail_tuple)):
             print(mail_tuple[i])
-            message = MIMEText('告警信息:%s' % message, 'plain', 'utf-8')
-            message['From'] = Header(u"服务器<%s>" % self.user)
-            message['To'] = Header(u"用户 <%s>" % mail_tuple[i])
-            message['Subject'] = Header(u"告警信息")
-            try:
-                smtpObj = smtplib.SMTP()
-                smtpObj.connect(self.server, 25)  # 25 为 SMTP 端口号
-                smtpObj.login(self.user, self.passwd)
-                smtpObj.sendmail(self.user, mail_tuple[i], message.as_string())
-                print("邮件发送成功")
-            except smtplib.SMTPException:
-                print("Error: 无法发送邮件")
+            print("message:\n%s " % ms)
+            # message = MIMEText('告警信息:%s' % ms, 'plain', 'utf-8')
+            # message['From'] = Header(u"服务器<%s>" % self.user)
+            # message['To'] = Header(u"用户 <%s>" % mail_tuple[i])
+            # message['Subject'] = Header(u"告警信息")
+            # try:
+            #     smtpObj = smtplib.SMTP()
+            #     smtpObj.connect(self.server, 25)  # 25 为 SMTP 端口号
+            #     smtpObj.login(self.user, self.passwd)
+            #     smtpObj.sendmail(self.user, mail_tuple[i], message.as_string())
+            #     print("邮件发送成功")
+            # except smtplib.SMTPException:
+            #     print("Error: 无法发送邮件")
 
 
 class Strategies():
@@ -73,13 +75,13 @@ class Strategies():
         if swap_precent > self.swap_precent:
             return 1, "交换分区使用率过高，内存不足"
         else:
-            return 0, "内存使用率正常"
+            return 0, "交换分区使用率正常"
 
     def check_diskio_data(self, diskio_precent):
         if diskio_precent > self.diskio_precent:
             return 1, "磁盘IO过高"
         else:
-            return 0, "磁盘正常"
+            return 0, "磁盘IO正常"
 
     def check_diskusage_data(self, diskusage_precent):
         if diskusage_precent > self.diskusage_precent:
@@ -89,9 +91,9 @@ class Strategies():
 
     def check_netio_data(self, netio_precent):
         if netio_precent > self.netio_precent:
-            return 1, "网络占用率过高"
+            return 1, "网络IO率过高"
         else:
-            return 0, "网络占用正常"
+            return 0, "网络IO正常"
 
     def check_user_data(self, user):
         rtu = "以下用户非法登录："
@@ -120,25 +122,45 @@ class Strategies():
         else:
             return 0, "端口开启正常"
 
+    def check_all_data(self, data):
+        """检测所有信息，并返回结果"""
+        cpu, cpu_message = self.check_cpu_data(data[0])
+        svmem, svmem_message = self.check_svmem_data(data[1])
+        swap, swap_message = self.check_swap_data(data[2])
+        diskio, diskio_message = self.check_diskio_data(data[3])
+        diskusage, diskusage_message = self.check_diskusage_data(data[4])
+        netio, netio_message = self.check_netio_data(data[5])
+        user, user_message = self.check_user_data(data[6])
+        port, port_message = self.check_port_data(data[7])
+        total = cpu + svmem + swap + diskio + diskusage + netio + user + port
+        message = cpu_message + '\n' + svmem_message + '\n' + swap_message + '\n' + diskio_message + '\n' + diskusage_message + \
+                  '\n' + netio_message + '\n' + user_message + '\n' + port_message + '\n'
+        return total, message
+
 
 if __name__ == '__main__':
     # data = {'1': 'zhoupans_mail@163.com', '2': 'zhoupan@xiyoulinux.org'}
     # a = Alam(data, 2, '一号机器CPU使用率过高！')
     # a.send_mail()
+    data = (0.0, 39.2, 0.0, 0.0, 24.2, 0.0, ['zhoupan'],
+            (63342, 80, 8307, 53, 22, 631, 443, 6942, 8000, 902, 3306, 8307, 631, 902))
     s = Strategies()
-    print(s.check_cpu_data(80))
-    print(s.check_cpu_data(91))
-    print(s.check_svmem_data(40))
-    print(s.check_svmem_data(93))
-    print(s.check_swap_data(0))
-    print(s.check_swap_data(11))
-    print(s.check_diskio_data(80))
-    print(s.check_diskio_data(94))
-    print(s.check_diskusage_data(40))
-    print(s.check_diskusage_data(71))
-    print(s.check_netio_data(20))
-    print(s.check_netio_data(91))
-    print(s.check_user_data(('zhoupan', 'root', 'ubuntu',)))
-    print(s.check_user_data(('zhoupan',)))
-    print(s.check_port_data((80, 8000, 22, 3306, 90,)))
-    print(s.check_port_data((80, 8000, 22, 3306,)))
+    total, message = s.check_all_data(data)
+    print(total)
+    print(message)
+    # print(s.check_cpu_data(80))
+    # print(s.check_cpu_data(91))
+    # print(s.check_svmem_data(40))
+    # print(s.check_svmem_data(93))
+    # print(s.check_swap_data(0))
+    # print(s.check_swap_data(11))
+    # print(s.check_diskio_data(80))
+    # print(s.check_diskio_data(94))
+    # print(s.check_diskusage_data(40))
+    # print(s.check_diskusage_data(71))
+    # print(s.check_netio_data(20))
+    # print(s.check_netio_data(91))
+    # print(s.check_user_data(('zhoupan', 'root', 'ubuntu',)))
+    # print(s.check_user_data(('zhoupan',)))
+    # print(s.check_port_data((80, 8000, 22, 3306, 90,)))
+    # print(s.check_port_data((80, 8000, 22, 3306,)))
