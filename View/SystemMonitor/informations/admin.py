405865e55739193ef09b09cbd935deb1e9eba02b
dreamer2018 <zhoupans_mail@163.com>
2016-10-08 22:14:15 +0800

    数据库建立

--- View/SystemMonitor/informations/admin.py
+++ View/SystemMonitor/informations/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from informations.models import *
 
 # Register your models here.
+admin.site.register(scputimes)
+admin.site.register(svmem)
+admin.site.register(sswap)
+admin.site.register(sdiskio)
+admin.site.register(sdiskusage)
+admin.site.register(snetio)
+admin.site.register(suser)
+admin.site.register(sport)
+admin.site.register(client)
+admin.site.register(receive)
+admin.site.register(alarm)
+admin.site.register(strategy)
+admin.site.register(user)
+admin.site.register(port)
+
--- View/SystemMonitor/informations/models.py
+++ View/SystemMonitor/informations/models.py
@@ -16,11 +16,168 @@ class scputimes(models.Model):
     irp = models.DecimalField(max_digits=16, decimal_places=2)
     softirq = models.DecimalField(max_digits=16, decimal_places=2)
     steal = models.DecimalField(max_digits=16, decimal_places=2)
-    guest = models.DecimalField(max_digits=16, decimal_places=2) 
+    guest = models.DecimalField(max_digits=16, decimal_places=2)
     guest_nice = models.DecimalField(max_digits=16, decimal_places=2)
+
 
 # /*=====================================================*/
 # /*     table: svmem            物理内存信息             */
 # /*=====================================================*/
-# class svmem(models.Model):
-#     total = models.
+class svmem(models.Model):
+    total = models.BigIntegerField()
+    available = models.BigIntegerField()
+    percent = models.DecimalField(max_digits=16, decimal_places=2)
+    used = models.BigIntegerField()
+    free = models.BigIntegerField()
+    active = models.BigIntegerField()
+    inactive = models.BigIntegerField()
+    buffers = models.BigIntegerField()
+    cached = models.BigIntegerField()
+    shared = models.BigIntegerField()
+
+
+# /*=====================================================*/
+# /*     table: sswap            虚拟内存信息             */
+# /*=====================================================*/
+
+class sswap(models.Model):
+    total = models.BigIntegerField()
+    used = models.BigIntegerField()
+    free = models.BigIntegerField()
+    percent = models.DecimalField(max_digits=16, decimal_places=2)
+    sin = models.IntegerField()
+    sout = models.IntegerField()
+
+
+# /*=====================================================*/
+# /*     table: sdiskio            磁盘IO                */
+# /*=====================================================*/
+
+
+class sdiskio(models.Model):
+    device = models.CharField(max_length=20, blank=True)
+    read_count = models.BigIntegerField()
+    write_count = models.BigIntegerField()
+    read_bytes = models.BigIntegerField()
+    write_bytes = models.BigIntegerField()
+    read_time = models.BigIntegerField()
+    write_time = models.BigIntegerField()
+    read_merged_count = models.BigIntegerField()
+    write_merged_count = models.BigIntegerField()
+    busy_time = models.BigIntegerField()
+
+
+# /*=====================================================*/
+# /*     table: sdiskusage            磁盘分区使用率      */
+# /*=====================================================*/
+
+class sdiskusage(models.Model):
+    point = models.CharField(max_length=20)
+    total = models.BigIntegerField()
+    used = models.BigIntegerField()
+    free = models.BigIntegerField()
+    percent = models.DecimalField(max_digits=16, decimal_places=2)
+
+
+# /*=====================================================*/
+# /*     table: snetio            网络IO                 */
+# /*=====================================================*/
+
+class snetio(models.Model):
+    device = models.CharField(max_length=20)
+    type = models.IntegerField()
+    bytes_sent = models.BigIntegerField()
+    bytes_recv = models.BigIntegerField()
+    packets_sent = models.BigIntegerField()
+    packets_recv = models.BigIntegerField()
+    errin = models.BigIntegerField()
+    errout = models.BigIntegerField()
+    dropin = models.BigIntegerField()
+    dropout = models.BigIntegerField()
+
+
+# /*=====================================================*/
+# /*     table: suser            用户信息                */
+# /*=====================================================*/
+
+class suser(models.Model):
+    type = models.IntegerField()
+    name = models.CharField(max_length=20)
+    terminal = models.CharField(max_length=20)
+    host = models.CharField(max_length=15)
+    started = models.DecimalField(max_digits=16, decimal_places=2)
+
+
+# /*=====================================================*/
+# /*     table: sport            端口信息                 */
+# /*=====================================================*/
+
+class sport(models.Model):
+    type = models.IntegerField()
+    port = models.IntegerField()
+
+
+# /*=====================================================*/
+# /*     table:client             客户端列表              */
+# /*=====================================================*/
+
+class client(models.Model):
+    host = models.CharField(max_length=15, unique=True)
+
+
+# /*=====================================================*/
+# /*     table: receive            接收到的信息           */
+# /*=====================================================*/
+
+class receive(models.Model):
+    client_id = models.IntegerField()
+    cpu_id = models.IntegerField()
+    svmem_id = models.IntegerField()
+    sswap_id = models.IntegerField()
+    diskio_id = models.IntegerField()
+    diskusage_id = models.IntegerField()
+    netio_type = models.IntegerField()
+    user_type = models.IntegerField()
+    port_type = models.IntegerField()
+
+
+# /*=====================================================*/
+# /*     table: alarm            警告信息                 */
+# /*=====================================================*/
+
+class alarm(models.Model):
+    recv_id = models.IntegerField()
+    client_id = models.IntegerField()
+    cpu = models.DecimalField(max_digits=16, decimal_places=2)
+    svmem = models.DecimalField(max_digits=16, decimal_places=2)
+    swap = models.DecimalField(max_digits=16, decimal_places=2)
+    diskio = models.DecimalField(max_digits=16, decimal_places=2)
+    diskusage = models.DecimalField(max_digits=16, decimal_places=2)
+    snetio = models.DecimalField(max_digits=16, decimal_places=2)
+    level = models.IntegerField()
+    message = models.CharField(max_length=200)
+
+
+# /*=====================================================*/
+# /*     table: strategy            告警策略             */
+# /*=====================================================*/
+
+class strategy(models.Model):
+    type = models.CharField(max_length=20)
+    argv = models.IntegerField()
+
+
+# /*=====================================================*/
+# /*     table: user            合法用户列表              */
+# /*=====================================================*/
+
+class user(models.Model):
+    name = models.CharField(max_length=255)
+
+
+# /*=====================================================*/
+# /*     table: port            合法端口列表              */
+# /*=====================================================*/
+
+class port(models.Model):
+    port = models.IntegerField()
