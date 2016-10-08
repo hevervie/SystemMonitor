4f6c1f06207a91070a60dc4f0f4cbe4ea3245ed9
dreamer2018 <zhoupans_mail@163.com>
2016-10-08 23:54:44 +0800

    add

--- View/SystemMonitor/SystemMonitor/urls.py
+++ View/SystemMonitor/SystemMonitor/urls.py
@@ -14,7 +14,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from informations.views import *
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^test/$',test)
 ]
