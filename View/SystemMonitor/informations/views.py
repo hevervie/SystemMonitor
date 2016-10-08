cf317e17264068f543fc322ff08d3943a40ecfc3
dreamer2018 <zhoupans_mail@163.com>
2016-10-08 23:55:11 +0800

    add

--- View/SystemMonitor/informations/views.py
+++ View/SystemMonitor/informations/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from informations.models import sdiskio
+
+
+def test(request):
+    diskio = sdiskio.objects.all()
+    html = ""
+    return HttpResponse(html)
 
 # Create your views here.
