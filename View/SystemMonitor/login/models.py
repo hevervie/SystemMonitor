8daccf6a2ca868a2c7c77b7edecd35d965d5521f
dreamer2018 <zhoupans_mail@163.com>
2016-10-11 00:17:06 +0800

    创建用户登录数据库

--- View/SystemMonitor/login/models.py
+++ View/SystemMonitor/login/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
+
+# /*=====================================================*/
+# /*     table: login            用户登录                */
+# /*=====================================================*/
+
+class login(models.Model):
+    name = models.CharField(max_length=20)
+    passwd = models.CharField(max_length=16)
+
+
+# /*=====================================================*/
+# /*     table: user            用户信息                */
+# /*=====================================================*/
+
+class user(models.Model):
+    user_id = models.IntegerField()
+    email = models.CharField(max_length=50)
