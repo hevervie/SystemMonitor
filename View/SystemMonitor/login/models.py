9422f340e693eb58c5d84f527f99824a26f38ee8
dreamer2018 <zhoupans_mail@163.com>
2016-10-13 09:59:18 +0800

    add

--- View/SystemMonitor/login/models.py
+++ View/SystemMonitor/login/models.py
@@ -14,6 +14,29 @@ class login(models.Model):
     def __unicode__(self):
         return self.name
 
+    def verification(self, username, password):
+        """用户名密码验证函数"""
+        try:
+            passwd = login.objects.get(name=username).passwd
+            print(passwd)
+            if passwd == password:
+                return 1, 'success'
+            else:
+                return 0, 'username or password error!'
+        except login.DoesNotExist as d:  # 如果不存在,则会抛出DoesNotExist异常
+            return -1, d
+        except login.MultipleObjectsReturned as m:  # 如果返回多个，则会抛出MultipleObjectsReturned异常
+            return -2, m
+        except Exception as e:
+            return -3, e
+
+    def get_all(self):
+        all_user = login.objects.all()
+        user = {}
+        for i in all_user:
+            user[i.name] = i.passwd
+        return user
+
 
 # /*=====================================================*/
 # /*     table: user            用户信息                */
--- View/SystemMonitor/login/views.py
+++ View/SystemMonitor/login/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from login.models import login
+from django.http import HttpResponseRedirect, HttpResponse
+
 
 # Create your views here.
+
+
+def index(request):
+    render = {
+        'url': '..'
+    }
+    return render_to_response('login.html', render, context_instance=RequestContext(request))
+
+
+def user_login(request):
+    request.encoding = 'utf-8'
+
+    if request.POST:
+        username = request.POST['username']
+        password = request.POST['password']
+
+        rtu, message = login().verification(username, password)
+        print(rtu, message)
+        if rtu > 0:
+            request.session['username'] = username
+            return HttpResponseRedirect('/root/')
+        elif rtu <= 0:
+            render = {
+                'url': '..',
+                'message': 'username or password not incorrect!'
+            }
+            return render_to_response('login.html', render, context_instance=RequestContext(request))
+    else:
+        render = {
+            'url': '..'
+        }
+        return render_to_response('login.html', render, context_instance=RequestContext(request))
