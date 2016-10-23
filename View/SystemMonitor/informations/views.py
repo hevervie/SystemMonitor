682a2bd4acf3e2e65fd6a0c6b110310c0a0543ba
dreamer2018 <zhoupans_mail@163.com>
2016-10-23 22:33:30 +0800

    测试ajax

--- View/SystemMonitor/informations/views.py
+++ View/SystemMonitor/informations/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render_to_response
 from django.http import HttpResponse
+from informations.models import *
+
 
 # Create your views here.
 def adm_index(request):
     pass
+
+
+def ajax_test(request):
+    cpu = scputimes.objects.all()[0,9]
+
+    for item in cpu:
+
+
