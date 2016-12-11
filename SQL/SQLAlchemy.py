5fb6e44b26bcc21b8be056bfa2ed0dfb7cba3974
dreamer2018 <zhoupans_mail@163.com>
2016-12-11 21:36:12 +0800

    success sql

--- SQL/SQLAlchemy.py
+++ SQL/SQLAlchemy.py
@@ -5,9 +5,8 @@
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, ForeignKey, UniqueConstraint, Index, DECIMAL, BIGINT, VARCHAR, CHAR, \
-    DATETIME
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, ForeignKey, DECIMAL, BIGINT, VARCHAR, DATETIME
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
 engine = create_engine("mysql+pymysql://root:root@127.0.0.1:3306/test", max_overflow=5)
@@ -15,8 +14,12 @@ engine = create_engine("mysql+pymysql://root:root@127.0.0.1:3306/test", max_over
 Base = declarative_base()
 
 
+# /*=====================================================*/
+# /*     table:  scputimes           CPU信息             */
+# /*=====================================================*/
+
 class Scputimes(Base):
-    __tablename__ = 'scputimes'
+    __tablename__ = 'informations_scputimes'
     id = Column(Integer, primary_key=True, autoincrement=True)
     user = Column(DECIMAL(16, 2), nullable=False)
     nice = Column(DECIMAL(16, 2), nullable=False)
@@ -30,8 +33,12 @@ class Scputimes(Base):
     guest_nice = Column(DECIMAL(16, 2), nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: svmem            物理内存信息             */
+# /*=====================================================*/
+
 class Svmem(Base):
-    __tablename__ = 'svmem'
+    __tablename__ = 'informations_svmem'
     id = Column(Integer, primary_key=True, autoincrement=True)
     total = Column(BIGINT, nullable=False)
     available = Column(BIGINT, nullable=False)
@@ -45,8 +52,12 @@ class Svmem(Base):
     shared = Column(BIGINT, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: sswap            虚拟内存信息             */
+# /*=====================================================*/
+
 class Sswap(Base):
-    __tablename__ = 'sswap'
+    __tablename__ = 'informations_sswap'
     id = Column(Integer, primary_key=True, autoincrement=True)
     total = Column(BIGINT, nullable=False)
     used = Column(BIGINT, nullable=False)
@@ -56,10 +67,14 @@ class Sswap(Base):
     sout = Column(Integer, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: sdiskio            磁盘IO                 */
+# /*=====================================================*/
+
 class Sdiskio(Base):
-    __tablename__ = 'sdiskio'
+    __tablename__ = 'informations_sdiskio'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    device = Column(VARCHAR(20), nullable=False)
+    device = Column(VARCHAR(20), nullable=True)
     read_count = Column(BIGINT, nullable=False)
     write_count = Column(BIGINT, nullable=False)
     read_bytes = Column(BIGINT, nullable=False)
@@ -71,8 +86,13 @@ class Sdiskio(Base):
     busy_time = Column(BIGINT, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: sdiskusage            磁盘分区使用率      */
+# /*=====================================================*/
+
+
 class Sdiskusage(Base):
-    __tablename__ = 'sdiskusage'
+    __tablename__ = 'informations_sdiskusage'
     id = Column(Integer, primary_key=True, autoincrement=True)
     point = Column(VARCHAR(20), nullable=False)
     total = Column(BIGINT, nullable=False)
@@ -81,8 +101,13 @@ class Sdiskusage(Base):
     percent = Column(DECIMAL(16, 2), nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: snetio            网络IO                 */
+# /*=====================================================*/
+
+
 class Snetio(Base):
-    __tablename__ = 'snetio'
+    __tablename__ = 'informations_snetio'
     id = Column(Integer, primary_key=True, autoincrement=True)
     device = Column(VARCHAR(20), nullable=False)
     type = Column(Integer, nullable=False)
@@ -96,49 +121,72 @@ class Snetio(Base):
     dropout = Column(BIGINT, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: suser            用户信息                */
+# /*=====================================================*/
+
 class Suser(Base):
-    __tablename__ = 'suser'
+    __tablename__ = 'informations_suser'
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(Integer, nullable=False)
     name = Column(VARCHAR(20), nullable=False)
     terminal = Column(VARCHAR(20), nullable=False)
-    host = Column(CHAR(15), nullable=False)
+    host = Column(VARCHAR(15), nullable=False)
     started = Column(DECIMAL(16, 2), nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: sport            端口信息                 */
+# /*=====================================================*/
+
+
 class Sport(Base):
-    __tablename__ = 'sport'
+    __tablename__ = 'informations_sport'
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(Integer, nullable=False)
     port = Column(Integer, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table:client             客户端列表              */
+# /*=====================================================*/
+
+
 class Client(Base):
-    __tablename__ = 'client'
+    __tablename__ = 'informations_client'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    host = Column(CHAR(15), unique=True, nullable=False)
+    host = Column(VARCHAR(15), unique=True, nullable=False)
+
+
+# /*=====================================================*/
+# /*     table: receive            接收到的信息           */
+# /*=====================================================*/
 
 
 class Receive(Base):
-    __tablename__ = 'receive'
+    __tablename__ = 'informations_receive'
     id = Column(Integer, primary_key=True, autoincrement=True)
     datetime = Column(DATETIME, nullable=False)
-    client_id = Column(Integer, ForeignKey('client.id'), nullable=False)
-    cpu_id = Column(Integer, ForeignKey('scputimes.id'), nullable=False)
-    svmem_id = Column(Integer, ForeignKey('svmem.id'), nullable=False)
-    sswap_id = Column(Integer, ForeignKey('sswap.id'), nullable=False)
-    diskio_id = Column(Integer, ForeignKey('sdiskio.id'), nullable=False)
-    diskusage_id = Column(Integer, ForeignKey('sdiskusage.id'), nullable=False)
-    netio = Column(Integer, nullable=False)
-    user = Column(Integer, nullable=False)
-    port = Column(Integer, nullable=False)
+    client_id = Column(Integer, ForeignKey('informations_client.id'), nullable=False)
+    cpu_id = Column(Integer, ForeignKey('informations_scputimes.id'), nullable=False)
+    svmem_id = Column(Integer, ForeignKey('informations_svmem.id'), nullable=False)
+    sswap_id = Column(Integer, ForeignKey('informations_sswap.id'), nullable=False)
+    sdiskio_id = Column(Integer, ForeignKey('informations_sdiskio.id'), nullable=False)
+    sdiskusage_id = Column(Integer, ForeignKey('informations_sdiskusage.id'), nullable=False)
+    snetio = Column(Integer, nullable=False)
+    suser = Column(Integer, nullable=False)
+    sport = Column(Integer, nullable=False)
 
+
+# /*=====================================================*/
+# /*     table: alarm            警告信息                 */
+# /*=====================================================*/
 
 class Alarm(Base):
-    __tablename__ = 'alarm'
+    __tablename__ = 'informations_alarm'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    receive = Column(Integer, ForeignKey('receive.id'), nullable=False)
-    client = Column(Integer, ForeignKey('client.id'), nullable=False)
+    receive_id = Column(Integer, ForeignKey('informations_receive.id'), nullable=False)
+    client_id = Column(Integer, ForeignKey('informations_client.id'), nullable=False)
     cpu = Column(DECIMAL(16, 2), nullable=False)
     svmem = Column(DECIMAL(16, 2), nullable=False)
     swap = Column(DECIMAL(16, 2), nullable=False)
@@ -149,21 +197,33 @@ class Alarm(Base):
     message = Column(VARCHAR(200), nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: strategy            告警策略             */
+# /*=====================================================*/
+
 class Strategy(Base):
-    __tablename__ = 'strategy'
+    __tablename__ = 'informations_strategy'
     id = Column(Integer, primary_key=True, autoincrement=True)
     type = Column(VARCHAR(20), nullable=False)
     argv = Column(Integer, nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: user            合法用户列表              */
+# /*=====================================================*/
+
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'informations_user'
     id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(VARCHAR(255), nullable=False)
 
 
+# /*=====================================================*/
+# /*     table: port            合法端口列表              */
+# /*=====================================================*/
+
 class Port(Base):
-    __tablename__ = 'port'
+    __tablename__ = 'informations_port'
     id = Column(Integer, primary_key=True, autoincrement=True)
     port = Column(Integer, nullable=False)
 
@@ -178,7 +238,7 @@ def drop_db():
     Base.metadata.drop_all(engine)
 
 
-#drop_db()
+drop_db()
 init_db()
 
 # 创建mysql操作对象
