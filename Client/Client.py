ff940d6b213b04317675a1b31708e689c9e4b086
dreamer2018 <zhoupans_mail@163.com>
2017-02-27 22:03:02 +0800

    对cpu等使用率客户端，减少服务器压力：客户端完成

--- Client/Client.py
+++ Client/Client.py
@@ -8,7 +8,7 @@
 import threading
 from socket import *
 from time import sleep
-
+import simplejson
 # 导入配置类
 from Configure import Configure
 # 导入配置类
@@ -35,18 +35,31 @@ class Client(threading.Thread):
 
     def run(self):
         """线程运行的方法，功能是每隔十秒钟，向服务器发送一下主机信息"""
+        sr = SystemResource
+        init_data = {
+            'cpu': sr.get_cpu_info(self),
+            'mem': sr.get_men_info(self),
+            'net': sr.get_net_info(self),
+            'disk': sr.get_disk_info(self),
+            'user': sr.get_user_info(self),
+            'port': sr.get_port_info(self)
+        }
+
         while True:
             tcpclinet = socket(AF_INET, SOCK_STREAM)
             tcpclinet.connect((self.host, self.port))
             sr = SystemResource()
             # 将列表数据转转换成字符串
-            data = sr.return_all_info()
+            data = sr.return_all_info(init_data)
             print(data)
-            data = data.__str__()
+            # 保存原旧数据
+            init_data = data
+            data = simplejson.dumps(data)
             # 将数据发送出去
             tcpclinet.send(data.encode())
             # 关闭连接
             tcpclinet.close()
+
             # 休眠十秒钟
             sleep(self.sleep_time)
 
--- Client/SystemResource.py
+++ Client/SystemResource.py
@@ -10,7 +10,6 @@
 import subprocess
 
 import psutil
-import simplejson
 
 BUF = 1025
 
@@ -22,12 +21,48 @@ class SystemResource():
         """类创建与初始化工作"""
         pass
 
-    def get_cpu_info(self):
+    def get_cpu_info(self, old=None):
         """获取cpu的信息"""
 
         # 获取CPU信息
         cpu_info = psutil.cpu_times()
-        return cpu_info
+        new = {
+            'user': cpu_info.user,
+            'nice': cpu_info.nice,
+            'system': cpu_info.system,
+            'idle': cpu_info.idle,
+            'iowait': cpu_info.iowait,
+            'irq': cpu_info.irq,
+            'softirq': cpu_info.softirq,
+            'steal': cpu_info.steal,
+            'guest': cpu_info.guest,
+            'guest_nice': cpu_info.guest_nice
+        }
+        if old == None:
+            return new
+
+        new_total = 0
+        old_total = 0
+
+        for k in old:
+            if k == 'cpu_percent':
+                continue
+            old_total += old[k]
+        for k in new:
+            if k == 'cpu_percent':
+                continue
+            new_total += new[k]
+
+        new_free = new['idle']
+        old_free = old['idle']
+
+        try:
+            cpu_percent = (1 - (new_free - old_free) / (new_total - old_total)) * 100
+        except ZeroDivisionError:
+            new['cpu_percent'] = 0
+        else:
+            new['cpu_percent'] = cpu_percent
+        return new
 
     def get_men_info(self):
         """获取内存信息"""
@@ -38,13 +73,31 @@ class SystemResource():
 
         # 以字典方式返回数据
         mem = {
-            'svmem': virt_mem_info,
-            'sswap': swap_mem_info,
+            'svmem': {
+                'total': virt_mem_info.total,
+                'available': virt_mem_info.available,
+                'percent': virt_mem_info.percent,
+                'used': virt_mem_info.used,
+                'free': virt_mem_info.free,
+                'active': virt_mem_info.active,
+                'inactive': virt_mem_info.inactive,
+                'buffers': virt_mem_info.buffers,
+                'cached': virt_mem_info.cached,
+                'shared': virt_mem_info.shared
+            },
+            'sswap': {
+                'total': swap_mem_info.total,
+                'used': swap_mem_info.used,
+                'free': swap_mem_info.free,
+                'percent': swap_mem_info.percent,
+                'sin': swap_mem_info.sin,
+                'sout': swap_mem_info.sout
+            },
         }
 
         return mem
 
-    def get_disk_info(self, mount_point="/"):
+    def get_disk_info(self, mount_point="/", old=None):
         """获取磁盘占用率和"""
 
         # 获取磁盘信息
@@ -52,33 +105,114 @@ class SystemResource():
         disk_usage = psutil.disk_usage(mount_point)
 
         # 以字典方式返回数据
-        disk = {
-            'disk_io': disk_io_count,
-            'disk_usage': disk_usage,
+        new = {
+            'disk_io': {
+                'read_count': disk_io_count.read_count,
+                'write_count': disk_io_count.write_count,
+                'read_bytes': disk_io_count.read_bytes,
+                'write_bytes': disk_io_count.write_bytes,
+                'read_time': disk_io_count.read_time,
+                'write_time': disk_io_count.write_time,
+                'read_merged_count': disk_io_count.read_merged_count,
+                'write_merged_count': disk_io_count.write_merged_count,
+                'busy_time': disk_io_count.busy_time
+            },
+            'disk_usage': {
+                'total': disk_usage.total,
+                'used': disk_usage.used,
+                'free': disk_usage.free,
+                'percent': disk_usage.percent
+            },
         }
+        if old == None:
+            return new
+        new_read = new['disk_io']['read_count']
+        old_read = old['disk_io']['read_count']
 
-        return disk
+        new_write = new['disk_io']['write_count']
+        old_write = old['disk_io']['write_count']
 
-    def get_net_info(self):
+        new_read_merg = new['disk_io']['read_merged_count']
+        old_read_merg = old['disk_io']['read_merged_count']
+
+        new_write_merg = new['disk_io']['write_merged_count']
+        old_write_merg = old['disk_io']['write_merged_count']
+
+        try:
+            diskio_percent = (new_read_merg - old_read_merg) / (new_read - old_read) + (
+                                                                                           new_write_merg - old_write_merg) / (
+                                                                                           new_write - old_write)
+        except ZeroDivisionError:
+            new['disk_io']['diskio_percent'] = 0
+        else:
+            new['disk_io']['diskio_percent'] = diskio_percent
+        return new
+
+    def get_net_info(self, old=None):
         """获取网络信息"""
 
         # 获取网络信息
         net_io_avrg = psutil.net_io_counters()
         net_io_count = psutil.net_io_counters(pernic=True)
-
+        net_count = {}
+        for k in net_io_count:
+            net_count[k] = {
+                'bytes_sent': net_io_count[k].bytes_sent,
+                'bytes_recv': net_io_count[k].bytes_recv,
+                'packets_sent': net_io_count[k].packets_sent,
+                'packets_recv': net_io_count[k].packets_recv,
+                'errin': net_io_count[k].errin,
+                'errout': net_io_count[k].errout,
+                'dropin': net_io_count[k].dropin,
+                'dropout': net_io_count[k].dropout
+            }
         # 以字典方式返回数据
-        net={
-            'net_avrg': net_io_avrg,
-            'net_count': net_io_count,
+        new = {
+            'net_avrg': {
+                'bytes_sent': net_io_avrg.bytes_sent,
+                'bytes_recv': net_io_avrg.bytes_recv,
+                'packets_sent': net_io_avrg.packets_sent,
+                'packets_recv': net_io_avrg.packets_recv,
+                'errin': net_io_avrg.errin,
+                'errout': net_io_avrg.errout,
+                'dropin': net_io_avrg.dropin,
+                'dropout': net_io_avrg.dropout
+            },
+            'net_count': net_count,
         }
-        return net
+
+        if old == None:
+            return new
+
+        new_sent = new['net_avrg']['bytes_sent']
+        old_sent = old['net_avrg']['bytes_sent']
+
+        new_recv = new['net_avrg']['bytes_recv']
+        old_recv = old['net_avrg']['bytes_recv']
+
+        try:
+            netio_precent = ((new_sent - old_sent) + (new_recv - old_recv) * 8) / 100 / 1024 / 1024 * 10
+        except ZeroDivisionError:
+            new['net_avrg']['netio_precent'] = 0
+        else:
+            new['net_avrg']['netio_precent'] = netio_precent
+
+        return new
 
     def get_user_info(self):
         """获取用户信息"""
 
         # 获取登陆用户信息
         user_info = psutil.users()
-        return user_info
+        users = {}
+        for i in range(0, len(user_info)):
+            users[i] = {
+                'name': user_info[i].name,
+                'terminal': user_info[i].terminal,
+                'host': user_info[i].host,
+                'started': user_info[i].started,
+            }
+        return users
 
     def get_port_info(self):
         """获取主机端口"""
@@ -96,21 +230,20 @@ class SystemResource():
                 pass
         return port
 
-    def return_all_info(self):
+    def return_all_info(self, old):
         """返回所有信息"""
 
         sr = SystemResource()
         data = {
-            'cpu': sr.get_cpu_info(),
+            'cpu': sr.get_cpu_info(old['cpu']),
             'mem': sr.get_men_info(),
-            'net': sr.get_net_info(),
-            'disk': sr.get_disk_info(),
+            'net': sr.get_net_info(old['net']),
+            'disk': sr.get_disk_info(old=old['disk']),
             'user': sr.get_user_info(),
             'port': sr.get_port_info()
         }
-
         # 返回所有数据
-        return simplejson.dumps(data)
+        return data
 
 
 if __name__ == '__main__':
