47b743b55804281ed6deccaea7a5ca5539b3fa3b
dreamer2018 <zhoupans_mail@163.com>
2017-03-07 20:22:46 +0800

    加入线程池技术

--- Test/ThreadPoolTest.py
+++ Test/ThreadPoolTest.py
@@ -3,3 +3,26 @@
 """
     Created by zhoupan on 3/7/17.
 """
+import threadpool
+import time
+import random
+
+
+def hello(self):
+    time.sleep(2)
+    print("test___ %s " % self['1'])
+    return "hello"
+
+
+def thread():
+    data = [{'1': 1, '2': 2}]
+    requests = threadpool.makeRequests(hello, data)
+    [pool.putRequest(req)
+     for req in requests]
+    pool.wait()
+
+
+pool = threadpool.ThreadPool(5)
+while True:
+    print("Test:")
+    thread()
