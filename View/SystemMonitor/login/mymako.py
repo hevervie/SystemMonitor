3a1f4635eaf4dfd53d1a7314ebbefc196ef64981
dreamer2018 <zhoupans_mail@163.com>
2017-03-29 21:22:25 +0800

    add

--- View/SystemMonitor/login/mymako.py
+++ View/SystemMonitor/login/mymako.py
@@ -15,9 +15,6 @@ from django.template.context import Context, RequestContext
 from mako.exceptions import TopLevelLookupException
 from mako.lookup import TemplateLookup
 
-from common.log import logger
-
-
 '''
 mylookup 也可以单独使用：
 @Example:
@@ -25,10 +22,12 @@ mylookup 也可以单独使用：
              mako_temp.render(**data)
 '''
 
+
 class AppTemplateLookup(TemplateLookup):
     """
     处理templates在application里的情况
     """
+
     def get_template(self, uri):
         try:
             return super(AppTemplateLookup, self).get_template(uri)
@@ -48,20 +47,22 @@ class AppTemplateLookup(TemplateLookup):
 
 
 mylookup = AppTemplateLookup(directories=settings.MAKO_TEMPLATE_DIR,
-                          module_directory=settings.MAKO_TEMPLATE_MODULE_DIR,
+                             module_directory=settings.MAKO_TEMPLATE_MODULE_DIR,
                              output_encoding='utf-8',
                              input_encoding='utf-8',
                              encoding_errors='replace',
                              collection_size=500,
-#                             cache_type='memcached',
-#                             cache_url='127.0.0.1:11211',
+                             #                             cache_type='memcached',
+                             #                             cache_url='127.0.0.1:11211',
                              )
-#==============================================================================
+
+
+# ==============================================================================
 # TemplateLookup的其他参数（提高性能的参数)：
 #    filesystem_checks=False,
 #    cache_type=memcached,
 #    cache_dir='', cache_url,
-#==============================================================================
+# ==============================================================================
 
 
 def render_mako(template_name, dictionary={}, context_instance=None):
@@ -89,19 +90,19 @@ def render_mako(template_name, dictionary={}, context_instance=None):
         context_instance = Context(dictionary)
     data = {}
     # construct date dictory
-    for d in context_instance:data.update(d)
+    for d in context_instance: data.update(d)
     # return response
-    return HttpResponse(mako_temp.render_unicode(**data))   # .replace('\r','').replace('\n','').replace('\t','')
+    return HttpResponse(mako_temp.render_unicode(**data))  # .replace('\r','').replace('\n','').replace('\t','')
 
 
 def render_mako_context(request, template_name, dictionary={}):
     '''render the mako template with the RequestContext and return the HttpResponse  '''
     context_instance = get_context_processors_content(request)
-    #===========================================================================
+    # ===========================================================================
     # # you can add csrf_token here
     # from django.core.context_processors import csrf
     # context_instance['csrf_token'] = csrf(request)['csrf_token']
-    #===========================================================================
+    # ===========================================================================
     # render
     return render_mako(template_name, dictionary=dictionary, context_instance=context_instance)
 
@@ -111,7 +112,6 @@ def render_mako_tostring(template_name, dictionary={}, context_instance=None):
     render_mako_tostring without RequestContext
     @note: 因为返回是string，所以这个方法适合include的子页面用
     '''
-
 
     mako_temp = mylookup.get_template(template_name)
     if context_instance:
@@ -125,7 +125,7 @@ def render_mako_tostring(template_name, dictionary={}, context_instance=None):
     for d in context_instance:
         data.update(d)
     # return string
-    return mako_temp.render_unicode(**data)     # .replace('\t','').replace('\n','').replace('\r','')
+    return mako_temp.render_unicode(**data)  # .replace('\t','').replace('\n','').replace('\r','')
 
 
 def render_mako_tostring_context(request, template_name, dictionary={}):
@@ -164,8 +164,6 @@ def get_context_processors_content(request):
         cp_func_list = tuple(import_string(path) for path in context_processors)
         for processors in cp_func_list:
             context.update(processors(request))
-    except Exception,e:
-        logger.error( "Mako: get_context_processors_content:%s" % e)
+    except Exception as e:
         context = Context()
     return context
-
--- View/SystemMonitor/login/views.py
+++ View/SystemMonitor/login/views.py
@@ -1,192 +1,227 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
 from login.models import login, user
 from django.http import HttpResponseRedirect, HttpResponse
+from login.mymako import render_mako_context
+import simplejson
 
 
-# Create your views here.
-
-
-def index(request):
+def login_index(request):
     render = {
-        'url': '.'
+        'url': '.',
+        'message': ''
     }
-    return render_to_response('login.html', render, context_instance=RequestContext(request))
+    return render_mako_context(request, 'login.html', render)
 
 
 def user_login(request):
     request.encoding = 'utf-8'
-
     if request.POST:
         username = request.POST['username']
         password = request.POST['password']
-
         rtu, message = login().verification(username, password)
         if rtu > 0:
             type = user().get_type_by_id(rtu)
-            request.session['username'] = username
-            if type == 0:
+            request.session['userid'] = user().get_user_by_id(rtu).id
+            if type == 1:  # 系统管理员
                 return HttpResponseRedirect('/root/')
-            elif type == 1:
+            elif type == 2:  # 高级运维人员
                 return HttpResponseRedirect('/employee/')
-            elif type == 2:
+            elif type == 3:  # 一般运维人员
                 return HttpResponseRedirect('/sysadm/')
             else:
                 return HttpResponse("Error")
-        elif rtu <= 0:
+        else:
             render = {
-                'url': '..',
                 'message': '用户名或密码错误！'
             }
-            return render_to_response('login.html', render, context_instance=RequestContext(request))
+            return render_mako_context(request, 'login.html', render)
     else:
         render = {
-            'url': '..'
+            'message': ''
         }
-        return render_to_response('login.html', render, context_instance=RequestContext(request))
+        return render_mako_context(request, 'login.html', render)
 
 
 def login_base(request, template, dict):
-    return render_to_response(template, dict)
+    return render_mako_context(request, template, dict)
 
 
 def login_passwd(request):
+    u = user().get_user_by_id(request.session['userid'])
     if request.POST:
-        user_num = request.session['username']
-        old_passwd = request.POST['old_passwd']
-        new_passwd = request.POST['new_passwd']
-        confirm_passwd = request.POST['confirm_passwd']
+        origin = request.POST['origin']
+        new = request.POST['new']
+        repeat = request.POST['repeat']
+        if new != repeat:
+            rtu = {
+                'status': False,
+                'message': "两次输入的密码不相同！"
+            }
+            return HttpResponse(simplejson.dumps(rtu))
+        result, mess = login().passwd_alter(u.id, new, origin)
+        if result == 0:
+            rtu = {
+                'status': False,
+                'message': mess
+            }
+            return HttpResponse(simplejson.dumps(rtu))
+        else:
+            rtu = {
+                'status': True,
+                'message': 'success'
+            }
+            return HttpResponse(simplejson.dumps(rtu))
 
-        rtu = user().is_num_exist(user_num)
-        if new_passwd != confirm_passwd:
-            return render_to_response('root/passwd.html', {'message': "两次输入的密码相同！"},
-                                      context_instance=RequestContext(request))
-        if rtu > 0:
-            rtu, message = login().passwd_alter(rtu, new_passwd, old_passwd)
-            if rtu == 1:
-                return render_to_response('root/passwd.html', {'message': user_num + "密码修改成功！"},
-                                          context_instance=RequestContext(request))
-            else:
-                return render_to_response('root/passwd.html', {'message': "密码修改失败！" + message},
-                                          context_instance=RequestContext(request))
-    return render_to_response('root/passwd.html', {}, context_instance=RequestContext(request))
+    return render_mako_context(request, 'root/passwd.html', {'users': u})
 
 
 def login_root(request):
-    type_1 = user().get_user_number_by_type(1)
-    type_2 = user().get_user_number_by_type(2)
-    return login_base(request, 'root/index.html', {'high_manager': type_1, 'common_manager': type_2})
+    userid = request.session['userid']
+    type_1 = user().get_user_number_by_type(2)
+    type_2 = user().get_user_number_by_type(3)
+    return render_mako_context(request, 'root/index.html',
+                               {'users': user().get_user_by_id(userid), 'high_manager': type_1,
+                                'common_manager': type_2})
 
 
 def login_add(request):
+    users = user().get_user_by_id(request.session['userid'])
     if request.POST:
         num = request.POST['num']
         name = request.POST['name']
         type = int(request.POST['type'])
-        email = request.POST['email']
+        email = request.POST['mail']
         if len(num) < 8 or len(num.strip()) < 8:
             message = "工号长度错误，须为8位数字！"
         elif len(name) < 1 or len(name) > 20:
             message = "姓名过长或过短！"
-        elif type != 1 and type != 2:
+        elif type != 1 and type != 2 and type != 3:
+            message = "角色类型不合法！"
+        elif len(email) < 1:
+            message = "邮箱不能为空！"
+        else:
+            usr = user()
+            if usr.is_num_exist(num) == 0:
+                usr.user_add(num, type, name, email)
+                print(num)
+                rtu = {
+                    'status': True,
+                    'message': 'success'
+                }
+                return HttpResponse(simplejson.dumps(rtu))
+            else:
+                message = num + "已存在，请重试！"
+                rtu = {
+                    'status': False,
+                    'message': message
+                }
+                return HttpResponse(simplejson.dumps(rtu))
+        rtu = {
+            'status': False,
+            'message': message
+        }
+        return HttpResponse(simplejson.dumps(rtu))
+    else:
+        return render_mako_context(request, 'root/add.html', {'users': users})
+
+
+def login_manage(request):
+    message = ""
+    u = user().get_user_by_id(request.session['userid'])
+    if request.POST:
+        user_id = int(request.POST['id'])
+        sign, message = user().delete_user_by_id(user_id)
+        users = user().get_all_user()
+        if sign == 1:
+            message = "删除成功！"
+        else:
+            message = "删除失败:"
+        return render_mako_context(request, 'root/manage.html',
+                                   {'alluser': users, 'users': u, 'message': message})
+    else:
+        pass
+        users = user().get_all_user()
+        return render_mako_context(request, 'root/manage.html',
+                                   {'alluser': users, 'users': u, 'message': message})
+
+
+def login_alter(request):
+    # 获取当前用户信息
+    u = user().get_user_by_id(request.session['userid'])
+    # 对信息进行更改
+    if request.POST:
+        user_id = request.POST['id']
+        num = request.POST['num']
+        name = request.POST['name']
+        type = int(request.POST['type'])
+        email = request.POST['mail']
+
+        if len(num) < 8 or len(num.strip()) < 8:
+            message = "工号长度错误，须为8位数字！"
+        elif len(name) < 1 or len(name) > 20:
+            message = "姓名过长或过短！"
+        elif type != 1 and type != 2 and type != 3:
             message = "角色类型不合法！"
         elif len(email) < 1:
             message = "邮箱不能为空！"
         else:
             u = user()
-            if u.is_num_exist(num) == 0:
-                u.user_add(num, type, name, email)
-                message = num + "添加成功！"
-                return render_to_response('root/add.html', {'message': message},
-                                          context_instance=RequestContext(request))
-            else:
-                message = num + "已存在，请重试！"
-                return render_to_response('root/add.html', {'message': message},
-                                          context_instance=RequestContext(request))
-        render = {
-            'message': message,
-            'num': num,
-            'name': name,
-            'email': email,
-        }
-        return render_to_response('root/add.html', render, context_instance=RequestContext(request))
-    else:
-        return render_to_response('root/add.html', {}, context_instance=RequestContext(request))
-
-
-def login_manage(request):
-    message = ""
-    if request.POST:
-        user_id = int(request.POST['id'])
-        sign, message = user().delete_user_by_id(user_id)
-        u = user().get_all_user()
-        if sign == 1:
-            message = "删除成功！"
-        else:
-            message = "删除失败:"
-        return render_to_response('root/manage.html',
-                                  {'user': u, 'type1': '一般运维', 'type2': '系统管理员', 'message': message,
-                                   'count': len(u) - 1},
-                                  context_instance=RequestContext(request))
-    else:
-        pass
-        u = user().get_all_user()
-        return render_to_response('root/manage.html',
-                                  {'user': u, 'type1': '一般运维', 'type2': '系统管理员', 'message': message,
-                                   'count': len(u) - 1},
-                                  context_instance=RequestContext(request))
-
-
-def login_alter(request):
-    render = {}
-    if request.POST:
-        user_id = request.POST['id']
-        if 'num' in request.POST.keys():
-            num = request.POST['num']
-            name = request.POST['name']
-            type = int(request.POST['type'])
-            email = request.POST['email']
-
-            render = {
-                'user_id': user_id,
-                'num': num,
-                'name': name,
-                'email': email,
-            }
-
-            if len(num) < 8 or len(num.strip()) < 8:
-                message = "工号长度错误，须为8位数字！"
-            elif len(name) < 1 or len(name) > 20:
-                message = "姓名过长或过短！"
-            elif type != 1 and type != 2:
-                message = "角色类型不合法！"
-            elif len(email) < 1:
-                message = "邮箱不能为空！"
-            else:
-                u = user()
-                sign, mess = u.user_alter(user_id, num, type, name, email)
-                if sign == 1:
-                    message = num + "修改成功！"
-                    render['message'] = message
-                else:
-                    message = num + "失败，请重试！"
-                    render['message'] = message
-            return render_to_response('root/alter.html', render, context_instance=RequestContext(request))
-        else:
-            u = user().get_user_by_id(user_id)
-            if u:
-                render = {
-                    'user_id': user_id,
-                    'num': u.user_num,
-                    'user_type': u.user_type,
-                    'name': u.name,
-                    'email': u.email,
+            sign, mess = u.user_alter(user_id, num, type, name, email)
+            if sign == 1:
+                rtu = {
+                    'status': True,
+                    'message': "success"
                 }
-            return render_to_response('root/alter.html', render, context_instance=RequestContext(request))
+                return HttpResponse(simplejson.dumps(rtu))
+            else:
+                rtu = {
+                    'status': False,
+                    'message': mess
+                }
+                return HttpResponse(simplejson.dumps(rtu))
+        rtu = {
+            'status': False,
+            'message': message
+        }
+        return HttpResponse(simplejson.dumps(rtu))
+
+    elif request.GET:
+        if 'id' in request.GET.keys():
+            usr = user().get_user_by_id(request.GET['id'])
+            render = {
+                'users': u,
+                'id': usr.id,
+                'user_num': usr.user_num,
+                'type': usr.user_type,
+                'name': usr.name,
+                'email': usr.email,
+            }
+            return render_mako_context(request, 'root/alter.html', render)
+        else:
+            pass
+    return HttpResponseRedirect("/root")
+
+
+def login_delete(request):
+    """对用户进行删除"""
+    if request.POST:
+        id = request.POST['id']
+        sign, mess = user().delete_user_by_id(id)
+        if sign == 1:
+            rtu = {
+                'status': True,
+                'message': "success"
+            }
+            return HttpResponse(simplejson.dumps(rtu))
+        else:
+            rtu = {
+                'status': False,
+                'message': mess
+            }
+            return HttpResponse(simplejson.dumps(rtu))
     else:
-        pass
-    return render_to_response('root/alter.html', render, context_instance=RequestContext(request))
+        return HttpResponseRedirect("/root/")
 
 
 def test(request):
-    return HttpResponse('success')
+    id = request.POST['id']
+    return HttpResponse(simplejson.dumps({'status': True, 'message': 'success'}))
